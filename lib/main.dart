d2de8171e83c70c8ae1c36f6362f6d79cef879e8
Mel Macario <xmelmacario@gmail.com>
2024-03-25 19:56:52 +0800

    added pause button functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Home(),
     );
   }
@@ -24,15 +25,46 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  void handleNavigateToVideo(videoUrl) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => VideoPlayerWidget(videoUrl: videoUrl),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Video mo to"),
+      appBar: AppBar(
+        title: const Text("Video mo to"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            MaterialButton(
+              onPressed: () {
+                handleNavigateToVideo("assets/kys.mp4");
+              },
+              child: const Text("Play first video"),
+              color: Colors.blue,
+              textColor: Colors.white,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            MaterialButton(
+              onPressed: () {
+                handleNavigateToVideo("assets/video2.mp4");
+              },
+              child: const Text("Play second video"),
+              color: Colors.black,
+              textColor: Colors.white,
+            )
+          ],
         ),
-        body: const Center(
-            child: VideoPlayerWidget(
-          videoUrl: "assets/video.mp4",
-        )));
+      ),
+    );
   }
 }
--- lib/video_player_widget.dart
+++ lib/video_player_widget.dart
@@ -12,6 +12,7 @@ class VideoPlayerWidget extends StatefulWidget {
 class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
   late VideoPlayerController _videoPlayerController;
   late Future<void> _initializeVideoPlayerFuture;
+  bool _isPlaying = true;
 
   @override
   void initState() {
@@ -33,26 +34,66 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
     super.dispose();
   }
 
+  void _stopVideo() {
+    setState(() {
+      _isPlaying = false;
+    });
+    _videoPlayerController.pause();
+  }
+
+  void _playVideo() {
+    setState(() {
+      _isPlaying = true;
+    });
+    _videoPlayerController.play();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _initializeVideoPlayerFuture,
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return SizedBox(
-                height: 300,
-                width: 500,
-                child: AspectRatio(
-                  aspectRatio: _videoPlayerController.value.aspectRatio,
-                  child: VideoPlayer(_videoPlayerController),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Video Player'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FutureBuilder(
+              future: _initializeVideoPlayerFuture,
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.done:
+                    return SizedBox(
+                      height: 300,
+                      width: 500,
+                      child: AspectRatio(
+                        aspectRatio: _videoPlayerController.value.aspectRatio,
+                        child: VideoPlayer(_videoPlayerController),
+                      ),
+                    );
+                  default:
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                }
+              },
+            ),
+            SizedBox(height: 16),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Container(
+                color: Colors.black,
+                child: IconButton(
+                  color: Colors.white,
+                  icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
+                  onPressed: _isPlaying ? _stopVideo : _playVideo,
                 ),
-              );
-            default:
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-          }
-        });
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
